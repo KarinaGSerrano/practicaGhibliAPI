7451d5c92123d0ab5815c71c932f61ccf6673e3c
KarinaGSerrano <karina.g.serrano@gmail.com>
2023-10-15 01:05:24 -0300

    vista detalle boton Ver Mas

--- main.js
+++ main.js
@@ -3,13 +3,12 @@
 const apiGhibli = fetch("https://ghibliapi.vercel.app/films")
   .then((response) => response.json())
   .then((data) => {
-    data.forEach((film) => {
-      console.log(film.title, film.description);
+    data.forEach((film, i) => {
       const containerCards = document.querySelector(".container-cards");
       const templateCard = `<div class="pt-4 col-lg-4 col-md-6 col-sm-12">
                               <div class="card text-center">
                                 <img
-                                  src="https://img.freepik.com/foto-gratis/gato-blanco-encuentra-rodillas-mujer_8353-539.jpg?w=1380&t=st=1697330825~exp=1697331425~hmac=8a774e2fbee7f40bbcf280e81f635fbe294f664acd282421c0c60625eb7ddf29"
+                                  src="${film.movie_banner}"
                                   class="card-img-top"
                                   alt="..."
                                 />
@@ -19,6 +18,7 @@ const apiGhibli = fetch("https://ghibliapi.vercel.app/films")
                                   <button
                                     type="button"
                                     class="btn btn-style"
+                                    id="film${i}"
                                     data-bs-toggle="modal"
                                     data-bs-target="#staticBackdrop"
                                   >
@@ -29,5 +29,20 @@ const apiGhibli = fetch("https://ghibliapi.vercel.app/films")
                             </div>`;
 
       containerCards.insertAdjacentHTML("beforeend", templateCard);
+
+      const btnVerMas = document.querySelector(`#film${i}`);
+      btnVerMas.addEventListener("click", () => {
+        const modalImg = document.querySelector(".modal-img > img");
+        modalImg.src = film.image;
+        const modalTitle = document.querySelector(".modal-title");
+        modalTitle.innerHTML = film.title;
+        const modalDato1 = document.querySelector(".modal-dato1");
+        modalDato1.innerHTML = film.director;
+        const modalDato2 = document.querySelector(".modal-dato2");
+        modalDato2.innerHTML = film.release_date;
+        const modalDato3 = document.querySelector(".modal-dato3");
+        modalDato3.innerHTML = film.running_time;
+      });
     });
+    return data;
   });
